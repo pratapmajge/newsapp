e536b647a6659c40a7343c50cb29af3b69e8c795
pratapmajge <majgepratap1504@gmail.com>
2025-03-05 16:03:02 +0530

    categorywise-fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import NavBar from './component/NavBar';
 import News from './component/News';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default class App extends Component {
-  // a=' Pratap';
   render() {
     return (
-      <div>
-        {/* Hello this is class based rect{this.a} */}
-        <NavBar />
-        <News pageSize={6} country='us' category="health"/>
-      </div>
-    )
+      <Router>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<News pageSize={6} country="us" category="general" />} />
+            <Route path="/business" element={<News pageSize={6} country="us" category="business" />} />
+            <Route path="/entertainment" element={<News pageSize={6} country="us" category="entertainment" />} />
+            <Route path="/general" element={<News pageSize={6} country="us" category="general" />} />
+            <Route path="/health" element={<News pageSize={6} country="us" category="health" />} />
+            <Route path="/science" element={<News pageSize={6} country="us" category="science" />} />
+            <Route path="/sports" element={<News pageSize={6} country="us" category="sports" />} />
+            <Route path="/technology" element={<News pageSize={6} country="us" category="technology" />} />
+          </Routes>
+        </div>
+      </Router>
+    );
   }
 }
-
-
-
-
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types'
 export class navbar extends Component {
 
@@ -7,38 +8,38 @@ export class navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">NewsApp</a>
+            <Link className="navbar-brand" to="/">NewsApp</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
+                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                 </li>
                 {/* <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/About">About</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/About">About</Link>
                 </li> */}
                 <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/bussiness">Bussiness</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/bussiness">Bussiness</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/entertainment">Entertainment</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/entertainment">Entertainment</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/general">General</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/general">General</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/health">Health</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/health">Health</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/science">Science</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/science">Science</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/sports">Sports</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/sports">Sports</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" rel="noreferrer"  href="/technology">Technology</a>
+                  <Link className="nav-link" rel="noreferrer"  to="/technology">Technology</Link>
                 </li>
               </ul>
             </div>
